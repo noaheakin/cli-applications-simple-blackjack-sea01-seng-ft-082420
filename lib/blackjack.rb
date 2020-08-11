44973ca7c5986b599cef6b375aa094460f32f3de
Noah Eakin <noaheakin@gmail.com>
2020-08-11 18:01:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,16 @@ end
 def hit? (card_total)
   prompt_user
   get_user_input
-  
+  binding.pry
+  if get_user_input == 'h'
+    deal_card
+    card_total += deal_card
+  elsif get_user_input == 's'
+    card_total
+  else
+    invalid_command
+    prompt_user
+  end
 end
 
 def invalid_command
