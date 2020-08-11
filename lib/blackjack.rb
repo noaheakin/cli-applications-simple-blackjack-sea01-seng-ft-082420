a5752aab56d8c2aee755c0e7ffa1419865c8a28e
Noah Eakin <noaheakin@gmail.com>
2020-08-11 19:39:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,7 +35,7 @@ def hit? (card_total)
   if user_input == 'h'
     card_total += deal_card
   elsif user_input == 's'
-    return "Thanks for playing!"
+    puts "Thanks for playing!"
   else
     invalid_command
     hit?(card_total)
